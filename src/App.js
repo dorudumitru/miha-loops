6617803b77c9f7bd6c77dc5e479ad9de736ab571
Doru Dumitru <doru.g.dumitru@gmail.com>
2020-11-20 10:51:37 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Container, Paper, TextField, Typography } from '@material-ui/core'
+import { useState } from 'react'
 
-function App() {
+function App () {
+  const [words, setWords] = useState('')
+  const [results, setResults] = useState([])
+  // const [result, setResult] = useState('')
+
+  const handleProcessWords = () => {
+    const wordsArray = words.split(',')
+    setResults(wordsArray)
+  }
+
+  const renderResult = (wordsArray) => {
+    if (wordsArray.length > 0) {
+      const finalResult = []
+      wordsArray.forEach(element => {
+        finalResult.push(element.trim())
+        finalResult.forEach(tw => {
+          if (tw.trim() !== element.trim()) { finalResult.push(`${tw.trim()}, ${element.trim()}`) }
+        })
+      })
+
+      return (
+        <Box>
+          <Paper elevation={3} style={{ padding: '2%' }}>
+            <Typography variant='h5' style={{ marginBottom: '8px' }}>
+              Results
+            </Typography>
+            <hr />
+            <ul>
+              {finalResult.map(res => {
+                return (
+                  <li key={res}>{res}</li>
+                )
+              })}
+            </ul>
+          </Paper>
+        </Box>
+      )
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container maxWidth='md' style={{ marginTop: '2%' }}>
+      <Box my={5}>
+        <Typography variant='h5' style={{ marginBottom: '8px' }}>
+          Enter all words separated by comma (",")
+        </Typography>
+        <TextField
+          style={{ marginBottom: '10px' }}
+          label='Ex: word1, word2, word3, ...'
+          variant='outlined'
+          fullWidth
+          onChange={(e) => setWords(e.target.value)}
+        />
+        <Button variant='contained' color='primary' onClick={handleProcessWords}>
+          Process Words
+        </Button>
+      </Box>
+      {renderResult(results)}
+    </Container>
+  )
 }
 
-export default App;
+export default App
